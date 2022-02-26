dcbf3b1fd574e39f87661484afdbc189fd6909d5
Robson De Andrade <theairelibre@protonmail.com>
2022-02-26 11:43:49 -0300

    Add script functions

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-project-webpage
+function toggler() {
+  var x = document.getElementById("items");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction(x) {
+  x.classList.toggle("fa-xmark");
+}
+// Thanks geeksforgeeks.org
